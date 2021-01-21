eb6efa1db6cb77dcfab0febd5d171503696fe054
github raamenzurururu <reku9696@gmail.com>
2021-01-21 15:17:06 +0900

    [add]first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+$(function () {
+  // TOPページボタン制御
+  $(".page-top").hide();
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".page-top").fadeIn();
+    } else {
+      $(".page-top").fadeOut();
+    }
+  });
+
+  $(".page-top").click(function () {
+    $(".body,html").animate({ scrollTop: 0 }, 500);
+    return false;
+  });
+});
